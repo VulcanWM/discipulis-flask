2441f542088a27be482a18d88adad6f0e1684d64
VulcanWM <logmedha09@gmail.com>
2023-01-26 21:20:35 +0000

    added an all nouns page

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template
+from vocab import nouns
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/all_nouns")
+def all_nouns():
+    nouns_dict = {}
+    for noun in nouns.keys():
+        latin_form = nouns[noun]
+        nominative = latin_form.split(",")[0]
+        clean = latin_form.replace(",", ", ")
+        nouns_dict[noun] = {"Nominative": nominative, "Clean": clean}
+    return render_template("all_nouns.html", nouns=nouns_dict)
