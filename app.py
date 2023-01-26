7bd192f78e059697042170b82541a5f7ca87540b
VulcanWM <logmedha09@gmail.com>
2023-01-26 21:38:50 +0000

    added an all verbs page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from vocab import nouns
+from vocab import nouns, verbs
 
 app = Flask(__name__)
 
@@ -18,3 +18,15 @@ def all_nouns():
         clean = latin_form.replace(",", ", ")
         nouns_dict[noun] = {"Nominative": nominative, "Clean": clean}
     return render_template("all_nouns.html", nouns=nouns_dict)
+
+
+@app.route("/all_verbs")
+def all_verbs():
+    verbs_dict = {}
+    for verb in verbs.keys():
+        latin_form = verbs[verb]
+        sing_1st = latin_form.split(",")[0]
+        clean = latin_form.replace(",", ", ")
+        verbs_dict[verb] = {"1stSingular": sing_1st, "Clean": clean}
+    print(verbs_dict)
+    return render_template("all_verbs.html", verbs=verbs_dict)
