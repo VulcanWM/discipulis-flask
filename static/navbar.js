0e7afd9967acb9140faf0c90756204cb1e2fa5b5
VulcanWM <logmedha09@gmail.com>
2023-01-31 20:57:13 +0000

    updated navbar links so that they work

--- static/navbar.js
+++ static/navbar.js
@@ -1,6 +1,6 @@
 function add_navbar(){
   var items = ['Home', "Nouns", "Verbs", "Convert Noun", "Convert Verb"]
-  var urls = ['/', '/nouns', '/verbs', '/convert_noun', 'convert_verb']
+  var urls = ['/', '/all_nouns', '/all_verbs', '/convert_noun', 'convert_verb']
   var navbar = document.getElementById("navbar")
   for (let i = 0; i < items.length; i++) {
     navbar.innerHTML += `<a href="${urls[i]}" class="navlink">${items[i]}</a>`
