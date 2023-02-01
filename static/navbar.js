5963352f151c01947c4a2940927eb891f4a1eec9
VulcanWM <logmedha09@gmail.com>
2023-02-01 17:13:02 +0000

    added Google Analytics

--- static/navbar.js
+++ static/navbar.js
@@ -1,3 +1,18 @@
+// Google Analytics
+
+var Script1 = document.createElement("script");
+Script1.setAttribute("async", null);
+Script1.src =
+"https://www.googletagmanager.com/gtag/js?id=G-LKMN9BPTZC";
+document.head.appendChild(Script1);
+
+var Script2 = document.createElement("script");
+Script2.innerText =
+"window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-LKMN9BPTZC');";
+document.head.appendChild(Script2);
+
+
+// Adding navigation bar
 function add_navbar(){
   var items = ['Home', "Nouns", "Verbs", "Convert Noun", "Convert Verb"]
   var urls = ['/', '/all_nouns', '/all_verbs', '/convert_noun', 'convert_verb']
@@ -7,6 +22,8 @@ function add_navbar(){
   }
 }
 
+
+// table functions
 function table_click(id, table){
     var cell_content = document.getElementById(id)
     var number = id.split(":")[0]
